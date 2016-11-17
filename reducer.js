f08344ed2efc20a42a27788a90dadcab7048c5fd
kfrn <knfrances@gmail.com>
2016-11-17 16:08:38 +1300

    refactored render.js

--- reducer.js
+++ reducer.js
@@ -3,14 +3,16 @@ module.exports = reducer
 
 function reducer (state, action){
   const newState = clone(state)
-  switch(action.type){
+  const { products, cart } = newState
+  const { type, payload } = action
+  switch(type){
     case 'ADD_TO_CART':
-      if (newState.cart[action.payload] == undefined || 0)newState.cart[action.payload] = 1
-      else newState.cart[action.payload] = newState.cart[action.payload] +1
+      if (cart[payload] == undefined || 0)cart[payload] = 1
+      else cart[payload] = cart[payload] +1
       return newState
     case 'REMOVE_FROM_CART':
-      if (newState.cart[action.payload] == undefined) return newState
-      else newState.cart[action.payload] = newState.cart[action.payload] -1
+      if (cart[payload] == undefined) return newState
+      else cart[payload] = cart[payload] -1
       return newState
     default:
       return newState
